2521512582e56621a6618c5821070a50b5bcfd55
Brock Tibert <btibert3@gmail.com>
2019-04-16 22:09:35 -0400

    most recent code -- zip parsing not working yet

--- pypeds/ipeds.py
+++ pypeds/ipeds.py
@@ -2,13 +2,29 @@ import pandas as pd
 import numpy as np
 import os
 from zipfile import ZipFile
+from urllib.request import urlopen
 
 
 # zip file factory
-def zip_parser():
-    # a utility to be used within packages
-    # this grabs the data, checks the contents, reads _rc else normal
-    return(True)
+def zip_parser(url):
+    assert isinstance(url, str), "url must be a string"
+    # help from https://svaderia.github.io/articles/downloading-and-unzipping-a-zipfile/
+    zipurl = url
+    # Download the file from the URL
+    zipresp = urlopen(zipurl)
+    # Create a new file on the hard drive
+    tempzip = open("/tmp/tempfile.zip", "wb")
+    # Write the contents of the downloaded file into the new file
+    tempzip.write(zipresp.read())
+    # Close the newly-created file
+    tempzip.close()
+    # Re-open the newly-created file with ZipFile()
+    zf = ZipFile("/tmp/tempfile.zip")
+    # Extract its contents into <extraction_path>
+    # note that extractall will automatically create the path
+    zf.extractall(path = '<extraction_path>')
+    # close the ZipFile instance
+    zf.close()
 
 # build a valid ipeds survey url
 def get_hd(year):
