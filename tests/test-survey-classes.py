68a891ca939a7d2f631d544749e0d684d6f35ce3
Brock Tibert <btibert3@gmail.com>
2019-05-14 11:27:52 -0400

    finish bug on out of index via unique tmp, fix ic for adm, cleanup

--- tests/test-survey-classes.py
+++ tests/test-survey-classes.py
@@ -1,9 +1,35 @@
 import pandas as pd
 from pypeds import ipeds
 
-## test ic
+## test multiple years
 YEARS = [2016,2017]
+
+## test ic
 ic = ipeds.IC(years=YEARS)
 ic.extract()
 df = ic.load()
+df.survey_year.value_counts()
+
+## test hd
+hd = ipeds.HD(years=YEARS)
+hd.extract()
+df = hd.load()
+df.survey_year.value_counts()
+
+## test sfa
+sfa = ipeds.SFA(YEARS)
+sfa.extract()
+df = sfa.load()
+df.survey_year.value_counts()
+
+## test efc
+efc = ipeds.EFC(YEARS)
+efc.extract()
+df = efc.load()
+df.survey_year.value_counts()
+
+## test icay
+icay = ipeds.ICAY(YEARS)
+icay.extract()
+df = icay.load()
 df.survey_year.value_counts()
