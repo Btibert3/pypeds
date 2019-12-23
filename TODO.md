0cbcc556ba9d763da8ebc5bead294da19e5e9d61
Brock Tibert <btibert3@gmail.com>
2019-12-23 18:44:59 -0500

    started master attributes view

--- TODO.md
+++ TODO.md
@@ -5,7 +5,7 @@ Needed a better place to organize my thoughts.
 
 - [x] finish CA view and test
 - [x] check discounting and add features/metrics
-- [] consider a master view that has metrics flattened (1 row per like cip codes, enrollment by state, etc.)
+- [] [STARTED] consider a master view that has metrics flattened (1 row per like cip codes, enrollment by state, etc.)
 - [] check docs
 - [] update README for views and tutorial
 - [] post package for initial release with flit or equivalent
--- pypeds/views.py
+++ pypeds/views.py
@@ -183,6 +183,7 @@ def program_completions(fall_years=[2017],
       hd_lower48 (bool): boolean (default = False) which if True, will only keep lower 48 states
       hd_regions (bool): boolean (default = True) which if True, will add state and region data at the institutional level
       hd_cols (list): a list of valid column names for the HD survey.  Only these columns will be returned.
+      degree_code (list): a list of valid values for the awlevel column in C_A (completions by program)
   """
 
   
@@ -210,6 +211,40 @@ def program_completions(fall_years=[2017],
   # return the data
   return (df)
 
+
+
+#================================================== master dataset of institution attributes
+## a dataframe with each rool as a school, and columns describing the institution
+def school_attributes(fall_years=[2017],
+                      hd_deg4yr = True,
+                      hd_service = False,
+                      hd_lower48 = False,
+                      hd_regions = True,
+                      hd_cols = ['unitid', 'fall_year', 'instnm', 'fips', 
+                                 'carnegie', 'sector', 'latitude', 'longitud'],
+                      ):
+  """
+  Build a dataset of school info and program completions.  
+
+  Parameters:
+      years (list): a list of integers for the survey years to include for the migration data
+      hd_deg4yr (bool): boolean (default = True) as to filter to only include degree-granting 4-year institutions
+      hd_service (bool): boolean (default = True) which if True, will remove US service schools
+      hd_lower48 (bool): boolean (default = False) which if True, will only keep lower 48 states
+      hd_regions (bool): boolean (default = True) which if True, will add state and region data at the institutional level
+      hd_cols (list): a list of valid column names for the HD survey.  Only these columns will be returned.
+  """  
+
+
+  # get the inst data
+  i = ipeds.HD(years=fall_years)
+  i.extract()
+  i.transform(deg4yr=hd_deg4yr)
+  i.transform(service=hd_service)
+  i.transform(lower_us=hd_lower48)
+  i.transform(regions=hd_regions)
+  i.transform(cols=hd_cols)
+  inst = i.load()
 
 
 
