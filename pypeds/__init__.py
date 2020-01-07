b8b870530d8344f43b7c8e7cf24b9787a9ea3db1
Brock Tibert <btibert3@gmail.com>
2020-01-06 21:40:53 -0500

    update package desc via init

--- pypeds/__init__.py
+++ pypeds/__init__.py
@@ -1,5 +1,5 @@
-"""A python package to help expedite insight generate from publicly available higher education datasets. """
-__version__ = '0.102'
+"""A python package to help facilitate the collection and analysis of education-related datasets. """
+__version__ = '0.103'
 
 from pypeds.ipeds import *
 from pypeds.datasets import *
