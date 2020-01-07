c53d3c7c82999c788e5fa9cf9caa519cf4a284f5
Brock Tibert <btibert3@gmail.com>
2020-01-06 19:47:11 -0500

    move version docstring to top of pypeds/init.py

--- pypeds/__init__.py
+++ pypeds/__init__.py
@@ -1,8 +1,9 @@
+"""A python package to help expedite insight generate from publicly available higher education datasets. """
+__version__ = '0.1'
+
 from pypeds.ipeds import *
 from pypeds.datasets import *
 from pypeds.views import *
 
 
-"""A python package to help expedite insight generate from publicly available higher education datasets. """
 
-__version__ = '0.1'
