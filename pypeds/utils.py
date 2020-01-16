4eb8c02a4a3eca75bd025fdb5c9175d25b3b8ffb
Brock Tibert <btibert3@gmail.com>
2020-01-16 15:49:21 -0500

    update to add tabpy server to utils module

---
+++ pypeds/utils.py
@@ -0,0 +1,26 @@
+import pickle
+import pandas as pd
+import altair as alt
+from tableausdk import *
+from tableausdk.HyperExtract import *
+
+
+def start_tabpy():
+  """
+  Start a tableau server in order to allow Tableau to call python for various functions
+  https://tableau.github.io/TabPy/docs/tabpy-virtualenv.html
+
+  Parameters:
+    
+  """
+  # simply start the server
+  tabpy
+  
+
+
+
+  
+  
+  
+  
+  
